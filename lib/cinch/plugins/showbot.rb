e842c996a2bc376e989a2b8fd4e07b8a516affe9
Jeremy Mack <jeremy@pileofturtles.com>
2011-06-22 22:29:38 -0400

    Added initial code for generic showbot commands Updated commands.rb to remove code as it is added to plugin

---
+++ lib/cinch/plugins/showbot.rb
@@ -0,0 +1,46 @@
+# Commands that didn't belong anywhere else
+
+module Cinch
+  module Plugins
+    class Showbot
+      include Cinch::Plugin
+
+      match %r{(help|commands) (.+)},   :method => :command_help    # !help
+      match %r{(about|showbot) (.+)},   :method => :command_about    # !about
+      match "uptime",   :method => :command_uptime    # !about
+      
+      def initialize(*args)
+        super
+        @start_time = Time.now
+      end
+
+      # Show help for the suggestions module
+      def command_help(m)
+        m.user.send [
+          "!about",
+          "!commands",
+          "!suggest",
+          "!suggestions" ]
+          # TODO add the rest here
+      end
+      
+      # Show information about showbot
+      def command_about(m)
+        m.user.send "Showbot was created by Jeremy Mack (@mutewinter) and some awesome contributors on github. The project page is located at https://github.com/mutewinter/Showbot"
+        m.user.send "Type !help for a list of showbot's commands"
+      end
+
+      # Tell them where to find the lovely suggestions
+      def command_uptime(m)
+        date_string = @start_time.strftime("%-m/%-d/%Y")
+        time_string = @start_time.strftime("%-I:%M%P")
+        seconds_running = ((Time.now - @start_time) * 24 * 60 * 60).to_i
+        m.user.send "Showbot has been running for " +
+          "#{ChronicDuration.output(seconds_running, :format => :long)} " +
+          "since #{date_string} at #{time_string}"
+      end
+
+    end
+  end
+end
+
--- lib/commands.rb
+++ lib/commands.rb
@@ -1,73 +1,12 @@
-require 'date'
-require 'chronic_duration'
-require 'ri_cal'
-require 'chronic'
+# this is no longer going to be used
+# things are being removed as they are added elsewhere
 
-require 'showbot/show.rb'
-require 'showbot/random.rb'
-require 'showbot/suggestion.rb'
-require 'showbot/events.rb'
 
-$domain = "http://5by5.tv"
-
-$drphil = ["There's a genie for that.",
-           "Everything's a bear.",
-           "A beret will be fine.",
-           "If you want to find the treasure you gotta buy the chest!",
-           "You don't win at tennis by buying a bowling ball.",
-           "If you live in a tree, don't be surprised that you're living with monkeys.",
-           "Crush the Bunny.",
-           "Doesn't matter how many Fords you buy, they're never gonna be a Dodge. You can repaint the Ford but... let's go to a break.",
-           "You're not gonna get Black Lung from an excel spreadsheet.",
-           "I'm not gonna euthanize this dog, I'm just gonna put it over here where I can't see it.",
-           "Failure is the equivalent of existential sit-ups."]
-
-$eight_ball = ["It is certain",
-               "It is decidedly so",
-               "Without a doubt",
-               "Yes - definitely",
-               "You may rely on it",
-               "As I see it, yes",
-               "Most likely",
-               "Outlook good",
-               "Signs point to yes",
-               "Yes",
-               "Reply hazy, try again",
-               "Ask again later",
-               "Better not tell you now",
-               "Cannot predict now",
-               "Concentrate and ask again",
-               "Don't count on it",
-               "My reply is no",
-               "My sources say no",
-               "Outlook not so good",
-               "Very doubtful"]
-$paleo = [
-  "You wouldn't be tired.",
-  "Your insulin wouldn't be spiking.",
-  "Elk.",
-  "No glutens."
-]
-
-$jsir = ["perl -le '$n=10; $min=5; $max=15; $, = \" \"; print map { int(rand($max-$min))+$min } 1..$n'",
-         "perl -le '$i=3; $u += ($_<<8*$i--) for \"127.0.0.1\" =~ /(\d+)/g; print $u'",
-         "perl -MAlgorithm::Permute -le '$l = [1,2,3,4,5]; $p = Algorithm::Permute->new($l); print @r while @r = $p->next'",
-         "perl -lne '(1x$_) !~ /^1?$|^(11+?)\\1+$/ && print \"$_ is prime\"'",
-         "perl -ple 's/^[ \\t]+|[ \\t]+$//g'"]
 
 $ical = "http://www.google.com/calendar/ical/fivebyfivestudios%40gmail.com/public/basic.ics"
 
 # Class to define the possible irc commands
 class Commands
-  @@command_usage = {
-    about: 'Who made this?',
-    description: '!description show_name episode_number',
-    suggest: '!suggest title_suggestion',
-    suggestions: '!suggestions [show|relative_time (e.g. 3 hours ago)]',
-    suggestion_count: 'Replies with the number of title suggestions showbot has collected.',
-    next: '!next [show_name]',
-    schedule: 'Prints a list of upcoming shows on 5by5'
-  }
 
   # Array to hold history of commands
   @@history = []
@@ -181,41 +120,7 @@ class Commands
   # --------------
   # Regular Commands
   # --------------
-  
-  # Replies to the user with a list of available commands for showbot
-  # !commands
-  def command_commands(args = [])
-    reply("Available commands:")
-    @@command_usage.each_pair do |command, usage|
-      reply("  !#{command} - #{usage}")
-    end
-  end
 
-  # Alias for the !commands command
-  def command_help(args = [])
-    command_commands(args)
-  end
-
-  # Replies to the user with information about showbot
-  def command_about(args = [])
-    reply("Showbot was created by Jeremy Mack (@mutewinter) and some awesome contributors on github. The project page is located at https://github.com/mutewinter/Showbot")
-    reply("Type !commands for showbot's commands")
-  end
-
-  # Alias for the about command
-  def command_showbot(args = [])
-    command_about(args)
-  end
-  
-  # Uptime command reports how long showbot has been running
-  def command_uptime(args = [])
-    date_string = @@start_time.strftime("%-m/%-d/%Y")
-    time_string = @@start_time.strftime("%-I:%M%P")
-    seconds_running = ((DateTime.now - @@start_time) * 24 * 60 * 60).to_i
-    reply("Showbot has been running for " +
-    "#{ChronicDuration.output(seconds_running, :format => :long)} " +
-    "since #{date_string} at #{time_string}")
-  end
 
   # Replies to the user with information about the next show
   # !next b2w -> The next Back to Work is in 3 hours 30 minutes (6/2/2011)
@@ -373,71 +278,7 @@ class Commands
     end
   end
 
-  # Clears the title suggestions
-  # !clear @@admin_key
-  def command_clear(args = [])
-    if args.first == @@admin_key
-      if @@suggestions.length == 1
-        reply("Clearing 1 title suggestion.")
-      elsif @@suggestions.length == 0
-        reply("There are no suggestions to clear. You can start adding some by using \"!suggest title_suggestion\".")
-      else
-        # Printing current suggestions so they aren't lost due to a malicious !clear
-        reply("Clearing #{@@suggestions.length} title suggestions.")
-      end
-      @@suggestions.clear
-    else
-      puts "Invalid admin key #{args.first}, should be #{@@admin_key}"
-    end
-  end
   
-  # --------------
-  # Fun commands
-  # --------------
-
-  def command_stopfailing(args = [])
-    chat("no.")
-  end
-
-  def command_merlin(args = [])
-    chat("SO angry.")
-  end
-
-  def command_drphil(args = [])
-    chat("\"#{$drphil.random}\"")
-  end
-
-  def command_sandy(args = [])
-    chat("He's great.")
-  end
-
-  def command_sandwich(args = [])
-    command_sandy(args)
-  end
-
-  def command_adam(args = [])
-    command_sandy(args)
-  end
-
-  def command_gruber(args = [])
-    chat("I don't know.")
-  end
-
-  def command_jsir(args = [])
-    chat($jsir.random)
-  end
-
-  def command_siracusa(args = [])
-    command_jsir(args)
-  end
-
-  def command_paleo(args = [])
-    chat($paleo.random)
-  end
-
-  def command_8ball(args = [])
-    chat("#{$eight_ball.random}.")
-  end
 
 end
 
