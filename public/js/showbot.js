d22d18eb3a8d689fb327fcdfea3afb92469f85d6
Jeremy Mack <jeremy@pileofturtles.com>
2011-06-30 21:22:04 -0400

    Made click also trigger hover for quotes
    Spent a stupid amount of time making this work. My god.

--- public/js/showbot.js
+++ public/js/showbot.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
   jQuery("abbr.timeago").timeago();
+
+  $('.suggestion').click(function(e) {
+    e.preventDefault();
+    $(this).toggleClass('hover_effect');
+  });
+
+  $('.suggestion').bind("mouseout", function(e) {
+    $(this).removeClass('hover_effect');
+  });
+  
 });
 
