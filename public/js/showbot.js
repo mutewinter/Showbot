a205e1b937f3f1c5bbb5f32f5c11487de12182ca
Jeremy Mack <jeremy@pileofturtles.com>
2011-07-25 22:20:06 -0400

    Removed hover event for touch devices
    Added Modernizr to detect touch devices

--- public/js/showbot.js
+++ public/js/showbot.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
   jQuery("abbr.timeago").timeago();
+  if (Modernizr.touch) {
+    // Remove hover events for Touch devices since they screw up rendering
+    $(".hover").removeClass("hover");
+  }
 });
 
