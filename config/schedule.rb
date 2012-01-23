1c7ff4089ec54a482c00014306b0371aeb097d25
Jeremy Mack <jeremy@pileofturtles.com>
2012-01-23 15:08:36 -0500

    Now logging crontab

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,24 +1,14 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+# schedule_production.rb
+# 
+# Whenever gem's crontab schedule for Showbot in production
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+set :environment, "production"
 
-# Learn more: http://github.com/javan/whenever
+set :output, '/var/log/showbot/crontab.log'
+
+# Fixed for Rack
+job_type :rake, "cd :path && RACK_ENV=:environment bundle exec rake :task --silent :output"
 
 every 1.day, :at => '3:00am' do
-  rake "backup:run"
+  rake 'backup:run'
 end
