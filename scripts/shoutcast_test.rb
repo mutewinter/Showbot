097c366320445f5fbbc3253c07e3a6f18a58ae3b
Jeremy Mack <jeremy@pileofturtles.com>
2012-08-20 22:53:53 -0400

    Added shoutcast test script

---
+++ scripts/shoutcast_test.rb
@@ -0,0 +1,31 @@
+require 'uri'
+require 'net/http'
+
+SHOUTCAST_URI = URI("http://5by5.fm/") 
+HEADERS = {
+  "Icy-MetaData" => '1'
+}
+
+
+# Fetches the show title from the live stream defined by URI
+def fetch_show
+  @last_update = Time.now
+
+  http = Net::HTTP.new(SHOUTCAST_URI.host, SHOUTCAST_URI.port)
+
+
+  chunk_count = 0
+  chunk_limit = 20 # Limit chunks to prevent lockups
+  http.get(SHOUTCAST_URI.path, HEADERS) do |chunk|
+    chunk_count += 1
+    if chunk =~ /StreamTitle='(.+?)';/
+      p chunk
+      return $1
+      break;
+    elsif chunk_count > chunk_limit
+      return nil
+    end
+  end
+end
+
+puts fetch_show
