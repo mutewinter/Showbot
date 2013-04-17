66f04331a864ab42522c46da73ad983a5354c36b
Jeremy Mack <mack@heysparkbox.com>
2013-04-17 18:50:42 -0400

    Fixes environment and twitter

--- lib/cinch/plugins/twitter.rb
+++ lib/cinch/plugins/twitter.rb
@@ -1,23 +1,22 @@
 # A Cinch plugin for broadcasting Twitter updates to an IRC channel
 
+require 'chronic_duration'
+require 'twitter'
+
 CHANNEL = "#5by5"
 TWITTER_USER = "5by5"
 
-require 'chronic_duration'
-require 'twitter'
+Twitter.configure do |config|
+  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+  config.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
+end
 
 module Cinch
   module Plugins
     class Twitter
       include Cinch::Plugin
-      
-      Twitter.configure do |config|
-        config.consumer_key = YOUR_CONSUMER_KEY
-        config.consumer_secret = YOUR_CONSUMER_SECRET
-        config.oauth_token = YOUR_OAUTH_TOKEN
-        config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
-      end
-
 
       timer 60, :method => :send_last_status
 
@@ -32,26 +31,24 @@ module Cinch
       # Send the last status for the TWITTER_USER to the user who requested it
       def command_last_status(m)
         begin
-          status = Twitter.user_timeline(TWITTER_USER).first
+          status = ::Twitter.user_timeline(TWITTER_USER).first
+          # Crazy hack fix for this line not sending a message for some reason
+          m.user.send 'Status:'
           m.user.send response_from_status(status)
-        rescue Twitter::Error::ServiceUnavailable
+        rescue ::Twitter::Error::ServiceUnavailable
           m.user.send "Oops, looks like Twitter's whale failed. Try again in a minute."
         end
       end
 
       def send_last_status
         begin
-          status = Twitter.user_timeline(TWITTER_USER).first
-        rescue Twitter::Error::ServiceUnavailable
+          status = ::Twitter.user_timeline(TWITTER_USER).first
+        rescue ::Twitter::Error::ServiceUnavailable
           puts "Error: Twitter is over capacity."
           return
         end
 
-        if @last_sent_id.nil?
-          # Skip the first message from TWITTER_USER so we don't spam every
-          # time the bot reconnects
-          @last_sent_id = status.id
-        elsif @last_sent_id != status.id
+        if @last_sent_id != status.id
 
           if status.in_reply_to_status_id or status.in_reply_to_screen_name
             # Don't show replies
@@ -60,9 +57,13 @@ module Cinch
 
           @last_sent_id = status.id
 
-          if development?
+          if @bot.nick =~ /_test$/
+            # Crazy hack fix for this line not sending a message for some reason
+            Channel("#cinch-bots").send 'Status Update:'
             Channel("#cinch-bots").send response_from_status(status)
           else
+            # Crazy hack fix for this line not sending a message for some reason
+            Channel(CHANNEL).send 'Status Update:'
             Channel(CHANNEL).send response_from_status(status)
           end
         end
--- readme.md
+++ readme.md
@@ -38,6 +38,17 @@ These commands will get you setup to run Showbot.
  * `bundle`
  * `rake db:migrate`
 
+Finally you need to setup your `.env` file in the root of the project. At the
+bare minimum you'll need the following:
+
+```
+RUBYLIB=./lib
+TWITTER_CONSUMER_KEY=your_info_here
+TWITTER_CONSUMER_SECRET=your_info_here
+TWITTER_OAUTH_TOKEN=your_info_here
+TWITTER_OAUTH_TOKEN_SECRET=your_info_here
+```
+
 ### Configuring IRC
 
  * Customize [`cinchize.yml`][cinchize] for your IRC channel.
