35a9b1b2268b7468e25d8252712966cc7d0ee44d
Richard Korman <winter.blood@gmail.com>
2014-02-09 08:02:55 -0500

    Update commands.rb to accept multi-user twitter

--- lib/cinch/plugins/commands.rb
+++ lib/cinch/plugins/commands.rb
@@ -24,7 +24,7 @@ module Cinch
           "!suggest - Be heard. Suggest a title for the live show.",
           "!link - Know the link for that? Suggest it and make the show better.",
           "!current - What's playing on #{shared[:Live_Url]}? I've got you covered.",
-          "!last_status - The last tweet by @#{shared[:Twitter_User]}, delievered to you in IRC. Sweet.",
+          "!last_status - The last tweet by @#{shared[:Twitter_User].join(", @")} delievered to you in IRC. Sweet.",
           "!about - Was #{shared[:Bot_Nick]} coded or did it spontaniously come into existence?",
           "!help - Uh, this.",
         ].join("\n")
